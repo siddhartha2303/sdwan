b55a8d200bd0a479cff53b0835fd917aeec09e03
Siddhartha Ghosh <siddhartha.ghosh1981@gmail.com>
2023-10-13 12:01:37 +0000

    changes

--- TERRAFORM/Device_Attach/main.tf
+++ TERRAFORM/Device_Attach/main.tf
@@ -20,21 +20,21 @@ resource "sdwan_attach_feature_device_template" "new_cedge" {
   devices = [{
     id = var.device_id
     variables = {
-        var_vpn0_internet_interface_name                               = var.var_vpn0_internet_interface_name  
-        var_vpn0_internet_interface_description                      = var.var_vpn0_internet_interface_description
-        var_vpn0_internet_interface_color                             = var.var_vpn0_internet_interface_color
-        var_vpn0_mpls_interface_name                              = var.var_vpn0_mpls_interface_name
-        var_vpn0_mpls_interface_description                       = var.var_vpn0_mpls_interface_description
-        var_vpn0_mpls_interface_color                             = var.var_vpn0_mpls_interface_color
-        var_vpn10_route_prefix                                  = var.var_vpn10_route_prefix
-        var_vpn10_route_nexthop                                = var.var_vpn10_route_nexthop
-        var_vpn512_interface_ip                     = var.var_vpn512_interface_ip
-        var_hostname                                     = var.var_hostname
-        var_system_ip                                    = var.var_system_ip
-        var_site_id                                      = var.var_site_id
-        var_service_vpn10_interface_name                 = var.var_service_vpn10_interface_name
-        var_service_vpn10_interface_description          = var.var_service_vpn10_interface_description
-        var_service_vpn10_interface_ip                   = var.var_service_vpn10_interface_ip
+      var_vpn0_internet_interface_name        = var.var_vpn0_internet_interface_name
+      var_vpn0_internet_interface_description = var.var_vpn0_internet_interface_description
+      var_vpn0_internet_interface_color       = var.var_vpn0_internet_interface_color
+      var_vpn0_mpls_interface_name            = var.var_vpn0_mpls_interface_name
+      var_vpn0_mpls_interface_description     = var.var_vpn0_mpls_interface_description
+      var_vpn0_mpls_interface_color           = var.var_vpn0_mpls_interface_color
+      var_vpn10_route_prefix                  = var.var_vpn10_route_prefix
+      var_vpn10_route_nexthop                 = var.var_vpn10_route_nexthop
+      var_vpn512_interface_ip                 = var.var_vpn512_interface_ip
+      var_system_hostname                     = var.var_system_hostname
+      var_system_ip                           = var.var_system_ip
+      var_site_id                             = var.var_site_id
+      var_service_vpn10_interface_name        = var.var_service_vpn10_interface_name
+      var_service_vpn10_interface_description = var.var_service_vpn10_interface_description
+      var_service_vpn10_interface_ip          = var.var_service_vpn10_interface_ip
     }
   }]
 }
--- TERRAFORM/Device_Attach/variable.tf
+++ TERRAFORM/Device_Attach/variable.tf
@@ -1,68 +1,70 @@
 variable "template_id" {
-    type = string
+  type = string
 }
 variable "template_version" {
-    type = string
+  type = string
 }
 variable "device_id" {
-    type = string
+  type = string
 }
 
 variable "var_vpn0_internet_interface_name" {
-  type    = string
+  type = string
 }
 
 variable "var_vpn0_internet_interface_description" {
-  type    = string
+  type = string
 }
 
 variable "var_vpn0_internet_interface_color" {
-  type    = string
+  type = string
 }
 
 variable "var_vpn0_mpls_interface_name" {
-  type    = string
+  type = string
 }
 
 variable "var_vpn0_mpls_interface_description" {
-  type    = string
+  type = string
 }
 
 variable "var_vpn0_mpls_interface_color" {
-  type    = string
+  type = string
 }
 
 variable "var_vpn10_route_prefix" {
-  type    = string
+  type = string
 }
 
 variable "var_vpn10_route_nexthop" {
-  type    = string
+  type = string
 }
 
 variable "var_vpn512_interface_ip" {
-  type    = string
+  type = string
 }
 
-variable "var_hostname" {
-  type    = string
+variable "var_system_hostname" {
+  type = string
 
 }
 
 variable "var_system_ip" {
-  type    = string
+  type = string
 }
 
 variable "var_service_vpn10_interface_name" {
-  type    = string
+  type = string
 }
 
 variable "var_service_vpn10_interface_description" {
-  type    = string
+  type = string
 }
 
 variable "var_service_vpn10_interface_ip" {
-  type    = string
+  type = string
 }
 
-variable "var_site_id" {}
+variable "var_site_id" {
+  type = string
+}
